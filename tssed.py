a9134e85e6fd0d23b243023d17ca3ecfdda3c5f3
Heyang Liu <lhypds@gmail.com>
2026-04-07 22:46:14 +0900

    Small fix

--- tssed.py
+++ tssed.py
@@ -22,7 +22,7 @@ def connect_and_watch_events(url):
 
             last_heartbeat = time.time()
 
-            for line in response.iter_lines(chunk_size=1, decode_unicode=True):
+            for line in response.iter_lines():
                 if not line:
                     continue
 
