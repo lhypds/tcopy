15f96878ecb94fd8c8614e2ba0d76360b882e1ff
Heyang Liu <lhypds@gmail.com>
2026-04-08 14:57:59 +0900

    Server copy

--- server_mode/server/copy.sh
+++ server_mode/server/copy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Server mode cannot use copy command."
+if [ -z "$1" ]; then
+    echo "Usage: $0 <text>"
+    exit 1
+fi
+
+echo "$1" > clipboard.txt
+echo "Text written to clipboard.txt: $1"
