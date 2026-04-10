73f08c00c7c5b16909617ac8cf8e6c108d8fb679
Heyang Liu <lhypds@gmail.com>
2026-04-10 11:47:15 +0900

    Debug

--- server_mode/client/client.js
+++ server_mode/client/client.js
@@ -157,11 +157,11 @@ app.get('/paste', async (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.flushHeaders();
 
   // Connect to the peer to trigger file transfer
   try {
-    await connectToPeer(peer, fromPeerId);
+    // await connectToPeer(peer, fromPeerId);
 
     // Send success event
     res.write(`data: ${JSON.stringify({ success: true, message: 'Connected to peer' })}\n\n`);
